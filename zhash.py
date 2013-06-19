53004e04181211c95c0236f3f377b8d6977df760
Angelo Righi <angelo.righi@gmail.com>
2013-06-19 22:45:46 +0200

    First commit

---
+++ zhash.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import getopt
+import hashlib
+import zipfile
+
+VERSION = '0.2'
+
+class Zhash:
+  verbose = False
+  hash = None
+  output_file = None
+  
+  def _sha1Checksum(self,filePath):
+      with open(filePath, 'rb') as fh:
+          m = hashlib.sha1()
+          while True:
+              data = fh.read(8192)
+              if not data:
+                  break
+              m.update(data)
+          return m.hexdigest().upper()
+
+  def _zipper(self,dir, zip_file):
+      zip = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED)
+      root_len = len(os.path.abspath(dir))
+      for root, dirs, files in os.walk(dir):
+          archive_root = os.path.abspath(root)[root_len:]
+          for d in dirs:
+                fullpath = os.path.join(root,d) + "\\"
+                archive_name = os.path.join(archive_root, d)
+                if self.verbose:
+                  print fullpath
+                zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+  
+          for f in files:
+              fullpath = os.path.join(root, f)
+              archive_name = os.path.join(archive_root, f)
+              if self.verbose:
+                print fullpath
+              zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+      zip.close()
+      return zip_file
+
+  def run(self,input_dir, output_file=None):
+    if os.path.exists(input_dir) == False:
+      raise Exception("Path %s non found" % input_dir)
+
+    if output_file == None:
+      output_file = input_dir 
+      
+    output_hash = output_file + '.sha1'
+    output_file += '.zip'
+
+    self._zipper(input_dir,output_file)
+    self.hash = self._sha1Checksum(output_file)
+    self.file = output_file
+    
+    with open(output_hash, "w") as text_file:
+      text_file.write("%s" % hash)
+  
+def usage():
+  script_name = os.path.basename(sys.argv[0])
+  print "Usage: %s -d <directory> [-v] [-h]" % script_name
+  print """  -d, --directory <directory>: Directory to zip
+  -o, --output <filename> (without extension): Output file
+  -v, --verbose: Verbosity
+  -h, --help: This help
+  """
+  
+def main():
+    verbose = False
+    input_dir = None
+    output_file = None
+
+    print "\nZhash v%s: Zip & Hash a directory" % VERSION
+
+    if len(sys.argv[1:]) == 0:
+      usage()
+      sys.exit(0)
+    
+    try:
+      options, remainder = getopt.getopt(sys.argv[1:], 'd:o:hv', ['directory=', 
+                                                             'output',
+                                                             'verbose',
+                                                             'help'
+                                                               ])
+    except getopt.GetoptError, err:
+		  print str(err) 
+		  usage()
+		  sys.exit(2)
+
+    for opt, arg in options:
+        if opt in ('-d', '--directory'):
+            input_dir = arg
+        elif opt in ('-v', '--verbose'):
+            verbose = True
+        elif opt in ('-o', '--output'):
+            output_file = arg
+        elif opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+
+    if input_dir == None:
+      usage()
+      sys.exit(2)
+
+    zhash = Zhash()
+    zhash.verbose = verbose
+    try:
+      print "Wait..."
+      zhash.run(input_dir,output_file)
+      print '%s -> %s' % (zhash.file,zhash.hash)
+      print "Done!"
+    except Exception, e:
+      print e
+      sys.exit(1)
+
+if __name__ == '__main__':
+  main()
